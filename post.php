6cd116c86b42c81737e311fd2f88cdc684c8b43c
nely de faria <nelly.nf54@gmail.com>
2021-11-11 15:50:22 -0300

    Corrigindo código

--- post.php
+++ post.php
@@ -12,43 +12,40 @@ if (isset($_GET['id'])) {
   }
 }
 ?>
-  <main id="post-container">
+    <main id="post-container">
     <div class="content-container">
       <h1 id="main-title"><?= $currentPost['title'] ?></h1>
       <p id="post-description"><?= $currentPost['description'] ?></p>
       <div class="img-container">
-        <img src="<?= $BASE_URL ?>/img/<?= $currentPost['img'] ?>" alt="<?= $currentPost['title'] ?>"
+        <img src="<?= $BASE_URL ?>/img/<?= $currentPost['img'] ?>" alt="<?= $currentPost['title'] ?>">
       </div>
-      <p class="post-content">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero ex enim aspernatur animi est sequi consectetur explicabo soluta error maxime, itaque quidem accusantium repellat ad, necessitatibus impedit, nulla qui deserunt.
-      Sunt obcaecati voluptas placeat similique quisquam magnam voluptatem! Perferendis eius et mollitia quia. Inventore fuga maxime beatae, aliquam molestiae aperiam at labore corrupti amet debitis commodi libero. Dignissimos, error non?
-      Amet, fugiat beatae eum, quidem distinctio saepe dolorem quae magni facere maiores necessitatibus magnam placeat, omnis quos recusandae tempora aspernatur alias. Reprehenderit ab dolor sint recusandae ea, vitae nemo odit.
-      Nam corporis explicabo, quo aperiam neque quaerat excepturi nulla alias magnam dolorum suscipit totam quibusdam autem reiciendis? Et ea a, fugiat repudiandae dolor aut temporibus reprehenderit error, libero quam enim.
-      Laboriosam, consectetur vitae. Similique, tempore facilis neque ipsam veniam reiciendis aut, quam labore quaerat eveniet dolores fugit eius quo maxime quia molestias vitae? Odit quaerat porro maxime eos ipsum illum.
-    </p>
-    <p class="post-content">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero ex enim aspernatur animi est sequi consectetur explicabo soluta error maxime, itaque quidem accusantium repellat ad, necessitatibus impedit, nulla qui deserunt.
-      Sunt obcaecati voluptas placeat similique quisquam magnam voluptatem! Perferendis eius et mollitia quia. Inventore fuga maxime beatae, aliquam molestiae aperiam at labore corrupti amet debitis commodi libero. Dignissimos, error non?
-      Amet, fugiat beatae eum, quidem distinctio saepe dolorem quae magni facere maiores necessitatibus magnam placeat, omnis quos recusandae tempora aspernatur alias. Reprehenderit ab dolor sint recusandae ea, vitae nemo odit.
-      Nam corporis explicabo, quo aperiam neque quaerat excepturi nulla alias magnam dolorum suscipit totam quibusdam autem reiciendis? Et ea a, fugiat repudiandae dolor aut temporibus reprehenderit error, libero quam enim.
-      Laboriosam, consectetur vitae. Similique, tempore facilis neque ipsam veniam reiciendis aut, quam labore quaerat eveniet dolores fugit eius quo maxime quia molestias vitae? Odit quaerat porro maxime eos ipsum illum.
-    </p>
+      <p class="post-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, explicabo provident, voluptatum, veniam nihil repellat eveniet quae adipisci exercitationem quos minus corrupti placeat veritatis architecto excepturi laudantium nulla tenetur cupiditate.
+      Odit saepe voluptas voluptates, iusto minima dolore deleniti corporis itaque, rem facere inventore in sed cumque voluptatibus unde! Assumenda nam aspernatur eveniet id illo inventore ratione laboriosam iusto culpa provident.
+      Nam blanditiis autem fugiat officiis animi, adipisci consequuntur minima. Mollitia atque iste sapiente quod pariatur necessitatibus minus voluptatem rerum eos modi enim perspiciatis provident ducimus, iure nostrum nobis eum iusto.
+      Perspiciatis sed numquam animi quae assumenda nesciunt voluptatibus rem! Deserunt, quae ex, pariatur nemo eveniet ipsam delectus aperiam aut quas blanditiis repellat quibusdam debitis sequi odit nostrum? Dolores, iure quam!
+      Beatae vel corrupti laborum repudiandae, placeat neque officiis odit provident reiciendis ducimus amet rem. Exercitationem similique itaque labore asperiores quasi officia aspernatur quas soluta quos. Quasi aliquam in laboriosam illum.</p>
+      <p class="post-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, explicabo provident, voluptatum, veniam nihil repellat eveniet quae adipisci exercitationem quos minus corrupti placeat veritatis architecto excepturi laudantium nulla tenetur cupiditate.
+      Odit saepe voluptas voluptates, iusto minima dolore deleniti corporis itaque, rem facere inventore in sed cumque voluptatibus unde! Assumenda nam aspernatur eveniet id illo inventore ratione laboriosam iusto culpa provident.
+      Nam blanditiis autem fugiat officiis animi, adipisci consequuntur minima. Mollitia atque iste sapiente quod pariatur necessitatibus minus voluptatem rerum eos modi enim perspiciatis provident ducimus, iure nostrum nobis eum iusto.
+      Perspiciatis sed numquam animi quae assumenda nesciunt voluptatibus rem! Deserunt, quae ex, pariatur nemo eveniet ipsam delectus aperiam aut quas blanditiis repellat quibusdam debitis sequi odit nostrum? Dolores, iure quam!
+      Beatae vel corrupti laborum repudiandae, placeat neque officiis odit provident reiciendis ducimus amet rem. Exercitationem similique itaque labore asperiores quasi officia aspernatur quas soluta quos. Quasi aliquam in laboriosam illum.</p>
     </div>
+    <aside id="nav-container">
+      <h3 id="tags-title">Tags</h3>
+      <ul id="tag-list">
+        <?php foreach($currentPost['tags'] as $tag): ?>
+          <li><a href="#"><?= $tag ?></a></li>
+        <?php endforeach; ?>
+      </ul>
+      <h3 id="categories-title">Categorias</h3>
+      <ul id="categories-list">
+        <?php foreach($categories as $category): ?>
+          <li><a href="#"><?= $category ?></a></li>
+        <?php endforeach; ?>
+      </ul>
+    </aside>
   </main>
-  <aside id="nav-container">
-    <h3 id="tags-title">Tags</h3>
-    <ul id="tags-list">
-      <?php foreach($currentPost['tags'] as $tag): ?>
-        <li><a href="#"><?= $tag ?></a></li>
-      <?php endforeach; ?>
-    </ul>
-    <h3 id="categories-title">Categorias</h3>
-    <ul id="categories-list">
-      <?php foreach($categories as $category): ?>
-        <li><a href="#"><?= $category ?></a></li>
-      <?php endforeach; ?>
-    </ul>
-  </aside>
+  
 <?php
 require_once 'templates/footer.php';
 ?>
